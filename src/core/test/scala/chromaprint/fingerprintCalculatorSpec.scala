053cd02139d0ab255a49d3a14b17291c8f358ed4
Mike Gibson <mike@mgdigital.co.uk>
2019-05-10 16:58:40 +0100

    Fixed test

--- src/core/test/scala/chromaprint/fingerprintCalculatorSpec.scala
+++ src/core/test/scala/chromaprint/fingerprintCalculatorSpec.scala
@@ -12,21 +12,22 @@ class fingerprintCalculatorSpec extends AbstractSpec {
     )
 
   it should "calculate a sub-fingerprint" in {
-    val image = Image(2)
+    val integral = Image(2)
       .addRow(Vector(0, 1))
       .addRow(Vector(2, 3))
-    val integral = image.integrate
+      .integrate
 
     fingerprintCalculator.subFingerprint(classifierConfig, integral, 0) should equal (fingerprintCalculator.grayCode(0))
     fingerprintCalculator.subFingerprint(classifierConfig, integral, 1) should equal (fingerprintCalculator.grayCode(2))
   }
 
   it should "calculate a fingerprint" in {
-    val image = Image(2)
+    val integral = Image(2)
       .addRow(Vector(0, 1))
       .addRow(Vector(2, 3))
       .addRow(Vector(4, 5))
+      .integrate
 
-      fingerprintCalculator(classifierConfig, image) should equal (Vector(0, 2, 3).map(fingerprintCalculator.grayCode))
+      fingerprintCalculator(classifierConfig, integral) should equal (Vector(0, 2, 3).map(fingerprintCalculator.grayCode))
   }
 }
