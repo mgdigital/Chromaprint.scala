e4dc7b553f61c347445b7e2501dd2ca146311e6a
Mike Gibson <mike@mgdigital.co.uk>
2019-05-20 17:19:21 +0100

    Use lazy vals for presets

--- src/core/main/scala/chromaprint/Presets.scala
+++ src/core/main/scala/chromaprint/Presets.scala
@@ -4,23 +4,23 @@ object Presets {
 
   // scalastyle:off magic.number
 
-  val algorithm1: Config =
+  lazy val algorithm1: Config =
     Config.default.copy(
       algorithm = 1,
       classifiers = ClassifierPresets(0)
     )
 
-  val algorithm2: Config =
+  lazy val algorithm2: Config =
     Config.default
 
-  val algorithm3: Config =
+  lazy val algorithm3: Config =
     Config.default.copy(
       algorithm = 3,
       classifiers = ClassifierPresets(2),
       interpolate = true
     )
 
-  val algorithm4: Config =
+  lazy val algorithm4: Config =
     Config.default.copy(
       algorithm = 4,
       silenceRemover = SilenceRemover.Config.default.copy(threshold = 50)
@@ -28,7 +28,7 @@ object Presets {
 
   // scalastyle:on magic.number
 
-  val map: Map[Int,Config] =
+  lazy val map: Map[Int,Config] =
     Seq(
       algorithm1,
       algorithm2,
@@ -41,7 +41,7 @@ object Presets {
   def apply(algorithm: Int): Config =
     map(algorithm)
 
-  val default: Config =
+  lazy val default: Config =
     algorithm2
 
   def exists(algorithm: Int): Boolean =
